11fdadbbe2f91ff0f342629e49fe38e89f51551c
Tiago Oliveira <44208665+Otiago06@users.noreply.github.com>
2021-09-24 19:05:37 -0300

    Create connection_database.php
    Database connection scheme

---
+++ assets/connection/connection_database.php
@@ -0,0 +1,21 @@
+<?php
+	setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+	date_default_timezone_set('America/Sao_Paulo');
+
+	session_start();
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "carwashes";
+	$app_versao = 'v0.0c';
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	  die("Connection failed: " . $conn->connect_error);
+	}
+
+	mysqli_query($conn,"SET NAMES 'utf8'");
+?>
