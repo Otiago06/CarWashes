8b05a2b002df4d184bd9a2487b6bc376f791a8c6
Tiago Oliveira <44208665+Otiago06@users.noreply.github.com>
2021-09-25 19:06:37 -0300

    Update entrada.php

--- entrada.php
+++ entrada.php
@@ -95,79 +95,14 @@
             <label for="formFileLg" class="form-label">Foto do Veículo</label>
             <div class="input-group">
               <input type="file" class="form-control" accept="image/*" id="uploadimage" name="uploadimage" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
-              <!--<button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">Enviar</button>-->
             </div>
           </div>
         </div>
       <hr><small class="text-muted">&nbsp;Serviços a serem realizados</small><br>
       <div class="container resultado"></div>
       <div class="h3 my-3 text-uppercase">Total dos serviços: <span id="total" class="text-info">R$ 0,00</span></div>
-
-      
-      <!--<div class="container">
-        <?php
-          $sql_servicos = "SELECT * FROM tb_servicos";
-          $result_servicos = $conn->query($sql_servicos);
-
-          if ($result_servicos->num_rows > 0) {
-            // output data of each row
-            while($row = $result_servicos->fetch_assoc()){
-              ?>
-                <div class="form-check">
-                  <input class="form-check-input h3" name='checkboxvar[]' type="checkbox" value="<?php echo $row['servico_id']; ?>" id="flexCheckDefault<?php echo $row['servico_id'] ?>">
-                  <label class="form-check-label h2" for="flexCheckDefault<?php echo $row['servico_id'] ?>">
-                    <?php echo $row['servico_descricao']; ?>
-                  </label>
-                </div>
-              <?php
-            }
-          }else{
-            ?>
-              <div class="text-center">
-                <div class="col">
-                  <small class="text-muted">Nenhum serviço foi cadastrado!</small>
-                </div>
-              </div>
-            <?php
-          }
-        ?>
-      </div>-->
-        
-        <!--<ul class="list-group list-group-flush">
-          <li class="list-group-item">
-            <div class="col">
-              <small class="text-muted">Lavagem detalhada</small>
-            </div>
-            <div class="col text-end">
-              <small class="text-muted">R$ 100.00</small>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="col">
-              <small class="text-muted">Revitalização de farol (par)</small>
-            </div>
-            <div class="col text-end">
-              <small class="text-muted">R$ 100.00</small>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="col">
-              <small class="text-muted">Polimento com revestimento cerâmico</small>
-            </div>
-            <div class="col text-end">
-              <small class="text-muted">R$ 800.00</small>
-            </div>
-          </li>
-        </ul>
-        <hr>
-        <div class="row mb-3">
-          <div class="col"><small class="text-muted"><strong>Total do(s) serviço(s):</strong></small></div>
-          <div class="col text-end"><small class="text-muted"><strong>R$ 1000.00</strong></small></div>
-        </div>        
-        <div class="d-grid gap-2 mb-3">
-          <button class="btn btn-dark btn-lg" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal"><small>SELECIONAR SERVIÇO(S)</small></button>
-        </div>-->
-      <hr><small class="text-muted">&nbsp;Dados de contato</small>
+      <hr>
+      <small class="text-muted">&nbsp;Dados de contato</small>
         <div class="row">
           <div class="col">
             <div class="form-floating mb-3">
@@ -256,8 +191,7 @@
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!---->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>    
     <script src="categorias_search.js"></script>
     <script src="placa_search.js"></script>
   </body>
