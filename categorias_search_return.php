36513615abe3a375f0d8d765782d81af7057e72f
Tiago Oliveira <44208665+Otiago06@users.noreply.github.com>
2021-09-25 19:06:31 -0300

    Update categorias_search_return.php

--- categorias_search_return.php
+++ categorias_search_return.php
@@ -1,8 +1,7 @@
-
 <?php
   include_once("assets/connection/connection_database.php");
 
-  $busca = $_POST['dropdownValue']['palavra']; //filter_input(INPUT_POST, 'palavra', FILTER_SANITIZE_STRING);
+  $busca = $_POST['dropdownValue']['palavra'];
   
   //Pesquisar no banco de dados nome do usuario referente a palavra digitada
   $result_servicos = "SELECT * FROM tb_servicos WHERE servico_categoria = $busca OR servico_categoria = 0 ORDER BY servico_id";
@@ -21,6 +20,7 @@
     echo '<li class="list-group-item text-center"><div class="col"><h4 class="text-muted">Não há serviços cadastrados!</h4></div></li>';
   }
 ?>
+
 <script>
     // Verifica clique em qualquer checkbox da tela
     $("input[type='checkbox']").click(function() {
@@ -33,7 +33,6 @@
                 total = total + parseFloat($(this).data("price"))
             }
         })
-
         // Exibe Total
         $("#total").html(total.toLocaleString('pt-BR', {
             style: 'currency',
@@ -42,20 +41,3 @@
         }))
     })
 </script>
-
-<!--
-<div class="form-check">
-            <input class="form-check-input" type="checkbox" value="<?php echo $row['servico_id'] ?>" data-price="<?php echo $row['servico_valor']; ?>">
-            <label class="form-check-label" for="srv_polimento">
-                <?php echo $row['servico_descricao']; ?>
-            </label>
-            <small class='text-info'>R$ <?php echo $row['servico_valor']; ?></small>
-        </div>
-
-<div class="form-check">
-          <input class="form-check-input h3" type="checkbox" name='checkboxvar[]' id="checkbox" data-price="<?php echo $row['servico_valor']; ?>" value="<?php echo $row['servico_id']; ?>">
-          <label class="form-check-label h3" for="flexCheckDefault<?php echo $row['servico_id'] ?>">
-            <span class="text-uppercase"><?php echo $row['servico_descricao']; ?> - <small class="text-info">R$ <?php echo $row['servico_valor']; ?></small></span>
-          </label>
-        </div>        
--->
