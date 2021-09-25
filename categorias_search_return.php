86674c1343cb365ea62802e9b0adea460e9b86a8
Tiago Oliveira <44208665+Otiago06@users.noreply.github.com>
2021-09-25 18:12:25 -0300

    Update categorias_search_return.php

--- categorias_search_return.php
+++ categorias_search_return.php
@@ -11,9 +11,9 @@
   if(($resultado_servicos) AND ($resultado_servicos->num_rows != 0 )){
     while($row = mysqli_fetch_assoc($resultado_servicos)){?>
         <div class="form-check">
-          <input class="form-check-input h3" name='checkboxvar[]' type="checkbox" value="<?php echo $row['servico_id']; ?>" id="checkbox<?php echo $row['servico_id'] ?>">
+          <input class="form-check-input h3" type="checkbox" name='checkboxvar[]' id="checkbox" data-price="<?php echo $row['servico_valor']; ?>" value="<?php echo $row['servico_id']; ?>">
           <label class="form-check-label h3" for="flexCheckDefault<?php echo $row['servico_id'] ?>">
-            <small class="text-uppercase"><?php echo $row['servico_descricao']; ?> - R$ <?php echo $row['servico_valor']; ?></small>
+            <span class="text-uppercase"><?php echo $row['servico_descricao']; ?> - <small class="text-info">R$ <?php echo @number_format($row['servico_valor'],2, ',', '.'); ?></small></span>
           </label>
         </div>
         <?php }
@@ -21,3 +21,41 @@
     echo '<li class="list-group-item text-center"><div class="col"><h4 class="text-muted">Não há serviços cadastrados!</h4></div></li>';
   }
 ?>
+<script>
+          // Verifica clique em qualquer checkbox da tela
+          $("input[type='checkbox']").click(function() {
+              var total = 0
+              // Percorre todos os checkboxes
+              $("input[type='checkbox']").each(function() {
+                  // Verifica se está selecionado
+                  if ($(this).is(':checked')) {
+                      // Incremente cálculo
+                      total = total + parseFloat($(this).data("price"))
+                  }
+              })
+
+              // Exibe Total
+              $("#total").html(total.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                  minimumFractionDigits: 2
+              }))
+          })
+      </script>
+
+<!--
+<div class="form-check">
+            <input class="form-check-input" type="checkbox" value="<?php echo $row['servico_id'] ?>" data-price="<?php echo $row['servico_valor']; ?>">
+            <label class="form-check-label" for="srv_polimento">
+                <?php echo $row['servico_descricao']; ?>
+            </label>
+            <small class='text-info'>R$ <?php echo $row['servico_valor']; ?></small>
+        </div>
+
+<div class="form-check">
+          <input class="form-check-input h3" type="checkbox" name='checkboxvar[]' id="checkbox" data-price="<?php echo $row['servico_valor']; ?>" value="<?php echo $row['servico_id']; ?>">
+          <label class="form-check-label h3" for="flexCheckDefault<?php echo $row['servico_id'] ?>">
+            <span class="text-uppercase"><?php echo $row['servico_descricao']; ?> - <small class="text-info">R$ <?php echo $row['servico_valor']; ?></small></span>
+          </label>
+        </div>        
+-->
