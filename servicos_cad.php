f9f31c9f37809ca9a0c747d0c25025c6d3057c55
Tiago Oliveira <44208665+Otiago06@users.noreply.github.com>
2021-09-25 18:13:33 -0300

    Update servicos_cad.php

--- servicos_cad.php
+++ servicos_cad.php
@@ -20,31 +20,7 @@
     
     <link rel="shortcut icon" href="assets/images/logo.png" type="image/x-icon">
 
-    <!-- Font Título -->
-    <style>
-      @font-face {
-        font-family: fontTitulo;
-        src: url(assets/fonts/paola.ttf);
-      }
-
-      .titulo {
-        font-family: fontTitulo;
-        font-size: 25px;
-      }
-
-      .recuo {
-        top: -5px;
-      }
-
-      .divider {
-        height: 4px;
-      }
-
-      body {
-        background-color: #04252d;
-      }
-    </style>
-
+    <link rel="stylesheet" href="style.css">
     <title>CarWashes</title>
   </head>
   <body>
@@ -54,7 +30,7 @@
         &nbsp;<a href="servicos.php" class="btn btn-link"><i class="fas fa-arrow-left fa-1x"></i></a>
       </span>
       <span class="navbar-text">
-        <a><span class="titulo">SERVICOS</span></a>
+        <a><span class="titulo">SERVIÇOS</span></a>
       </span>
       <span class="navbar-text">
         &nbsp;
